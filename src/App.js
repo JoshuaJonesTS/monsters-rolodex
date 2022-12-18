24e8642adc6bea9b133ab7b2c31f3a53c598fda4
JoshuaJonesTS <joshua@tailoredsoftware.us>
2022-12-17 17:31:41 -0800

    Create button

--- src/App.js
+++ src/App.js
@@ -12,14 +12,9 @@ class App extends Component {
           <p>
             Hello Joshua
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <button>
+            Change Name
+          </button>
         </header>
       </div>
     );
