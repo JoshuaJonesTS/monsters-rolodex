3917354824a36c0b69443ce9051a0b60e12b9557
JoshuaJonesTS <joshua@tailoredsoftware.us>
2022-12-19 18:37:55 -0800

    Pseudocode

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ class App extends Component {
       <div className="App">
         {
           this.state.monsters.map((monster) => {
-            return <h1 key={monster.id}>{monster.name}</h1>
+            return <h1 key={monster.id}>{monster.name}</h1> // the key is a unique identifier
           })
         }
       </div>
