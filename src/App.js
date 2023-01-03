4662651ac4b04233b87c83557c7b18cde05ddda6
JoshuaJonesTS <joshua@tailoredsoftware.us>
2023-01-03 14:24:41 -0800

    Search bar functionality

--- src/App.js
+++ src/App.js
@@ -33,7 +33,20 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <input className='search-box' type='search' placeholder='search monsters' onChange={(event) => {}}/>
+        <input 
+          className='search-box' 
+          type='search' 
+          placeholder='search monsters' 
+          onChange={(event) => {
+            const searchString = event.target.value.toLocaleLowerCase();
+            const filteredMonsters = this.state.monsters.filter((monster) => {
+              return monster.name.toLocaleLowerCase().includes(searchString);
+            });
+
+            this.setState(() => {
+              return {monsters: filteredMonsters };
+            });
+          }}/>
         {
           this.state.monsters.map((monster) => {
             return <div key={monster.id}><h1>{monster.name}</h1></div> // the key is a unique identifier
