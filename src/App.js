d4814384f9ae748c75fe041a9fbe9307e60c9fae
JoshuaJonesTS <joshua@tailoredsoftware.us>
2023-01-23 12:49:21 -0800

    Use useState hook

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
-import { Component } from 'react';
-// import logo from './logo.svg';
+import { useState } from 'react'
 import CardList from './components/card-list/card-list.component';
 import SearchBox from './components/search-box/search-box.component';
 import './App.css';
 
 const App = () => {
+  const [searchField, setSearchField] = useState(''); // useState gives an array [value, setValue]
+  
+  const onSearchChange = () => {
+    const searchFieldString = event.target.value.toLocalLowerCase();
+    setSearchField(searchFieldString);
+  }
+  
   return (
     <div className="App">
       <h1 className='app-title'>Monsters Rolodex</h1>
-      {/* <SearchBox 
+      <SearchBox 
         className='monsters-search-box'
         onChangeHandler={onSearchChange} 
         placeholder='search montsers'/>
-      <CardList monsters={filteredMonsters}/> */}
+      <CardList monsters={filteredMonsters}/>
     </div>
   );
 }
